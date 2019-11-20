53ea2b1ce2a2e3452e905861d35575f2ce913036
Guilherme Chaguri <guichaguri@gmail.com>
2019-11-20 10:30:57 -0300

    Added NVIDIA Jetson I2C support

--- Adafruit_GPIO/I2C.py
+++ Adafruit_GPIO/I2C.py
@@ -53,6 +53,9 @@ def get_default_bus():
     elif plat == Platform.BEAGLEBONE_BLACK:
         # Beaglebone Black has multiple I2C buses, default to 1 (P9_19 and P9_20).
         return 1
+    elif plat == Platform.JETSON_NANO:
+        # Defaults to 1 on the NVIDIA Jetson
+        return 1
     else:
         raise RuntimeError('Could not determine default I2C bus for platform.')
 
